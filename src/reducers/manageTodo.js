a0fc0db84289edf54aa8a392e66482e4eb7ac300
mjaber <abujaber44@gmail.com>
2021-03-23 11:24:01 -0400

    commit tree e7e2adce8a8438642f913322f687ccf3e1630d52

--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -1,8 +1,21 @@
-export default function manageTodo(state = {
-  todos: [],
-}, action) {
 
-  console.log("reducer received this action:", action);
+export default function manageTodo(state = {todos: [], }, action) {
 
-  return state;
-}
+  state = {
+    todos: [
+      { text: 'buy groceries' },
+      { text: 'watch netflix' },
+    ]
+  } 
+  
+  switch (action.type) {
+    case 'ADD_TODO':
+
+      console.log({ todos: state.todos.concat(action.payload.text) });
+
+      return { todos: state.todos.concat(action.payload.text) };
+
+    default:
+      return state;
+  }
+} 
